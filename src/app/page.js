707002507b4f101820c04868d2366f0045f53e71
unknown <joheandroid@gmail.com>
2023-09-21 18:01:06 -0300

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,16 @@ import Image from 'next/image'
 
 import Header from '@/components/Header'
 import Kpis from '@/components/Kpis'
+import NameUser from '@/components/NameUser'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen ">
-         <Kpis /> 
+    <main className="min-h-screen " >
+        <div class="pt-10 px-10">
+          <NameUser />
+          <Kpis /> 
+        </div>
     </main>
   )
 }
---
+++ src/components/NameUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NameUser() {
+  return (
+    <div className='mb-6'>
+        <h1 class="text-bold text-xl"> Hi, Wendys R. asdasdasd </h1>
+    </div>
+  )
+}
